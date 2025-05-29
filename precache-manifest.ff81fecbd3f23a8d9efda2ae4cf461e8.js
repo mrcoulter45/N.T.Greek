d53a05ceb09005a57d3e4676ab6886d8eb016f78
Michael Coulter <michael.coulter@arm.com>
2025-05-29 16:08:46 -0500

    Updates

---
+++ precache-manifest.ff81fecbd3f23a8d9efda2ae4cf461e8.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656831224fe046e57f2d3a0dc52ccb25",
+    "url": "/nt-greek/index.html"
+  },
+  {
+    "revision": "198b0f2856e2038ea86a",
+    "url": "/nt-greek/static/css/2.dcbc423f.chunk.css"
+  },
+  {
+    "revision": "c8cc1f3ebbc07cf3a4b4",
+    "url": "/nt-greek/static/css/main.cfe14a5d.chunk.css"
+  },
+  {
+    "revision": "198b0f2856e2038ea86a",
+    "url": "/nt-greek/static/js/2.adde7c63.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/nt-greek/static/js/2.adde7c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cc1f3ebbc07cf3a4b4",
+    "url": "/nt-greek/static/js/main.58eb754d.chunk.js"
+  },
+  {
+    "revision": "df977845b8613ff5c37c",
+    "url": "/nt-greek/static/js/runtime-main.3216e493.js"
+  },
+  {
+    "revision": "370891cb90b49bb6fded7962726fe50f",
+    "url": "/nt-greek/static/media/adjectives_img.370891cb.JPG"
+  },
+  {
+    "revision": "d79c0efcfb254e9af8b0c83fa723c550",
+    "url": "/nt-greek/static/media/alphabet_img.d79c0efc.JPG"
+  },
+  {
+    "revision": "3bec71325aa84d0cef0885576fe9fbff",
+    "url": "/nt-greek/static/media/article_img.3bec7132.JPG"
+  },
+  {
+    "revision": "995e4137669f6f187818bb3327dc2f87",
+    "url": "/nt-greek/static/media/contraction0_img.995e4137.JPG"
+  },
+  {
+    "revision": "3ddec51a6b4fb9207040f288bb8bf106",
+    "url": "/nt-greek/static/media/contraction1_img.3ddec51a.JPG"
+  },
+  {
+    "revision": "af03fbff68182703102e71a8e297b9f5",
+    "url": "/nt-greek/static/media/demonstrative_adjectives_img.af03fbff.JPG"
+  },
+  {
+    "revision": "80c54093693d19b7f3bcffd37bcd7df8",
+    "url": "/nt-greek/static/media/first_declension_imperf_alpha_img.80c54093.JPG"
+  },
+  {
+    "revision": "4d8ce9b7431404b8bc1bb28f0a3b9cf0",
+    "url": "/nt-greek/static/media/first_declension_perf_alpha_img.4d8ce9b7.JPG"
+  },
+  {
+    "revision": "22a6253cfe3f0b38332282e78829cdbf",
+    "url": "/nt-greek/static/media/first_declension_perf_eta_img.22a6253c.JPG"
+  },
+  {
+    "revision": "fa9dfbbba03f8e2c6d9b592213a1c520",
+    "url": "/nt-greek/static/media/fut_liquid_verbs_img.fa9dfbbb.JPG"
+  },
+  {
+    "revision": "db5bce13cf58a36ffc1e9fffbd6fd055",
+    "url": "/nt-greek/static/media/gen_abs.db5bce13.JPG"
+  },
+  {
+    "revision": "c243d0e13ff53fdc3dd92e2bd36901ca",
+    "url": "/nt-greek/static/media/gen_abs_ex0.c243d0e1.JPG"
+  },
+  {
+    "revision": "fd3bc4f1b39f7578956d2c7e2a172b02",
+    "url": "/nt-greek/static/media/gen_abs_ex1.fd3bc4f1.JPG"
+  },
+  {
+    "revision": "4994204bf043d4552f3f8da9c277efa5",
+    "url": "/nt-greek/static/media/general_rules_of_accent_img.4994204b.JPG"
+  },
+  {
+    "revision": "b43c8d9b627ce2486eeaada1402291b9",
+    "url": "/nt-greek/static/media/genitive_of_comparison_img.b43c8d9b.JPG"
+  },
+  {
+    "revision": "bfbf70320074121c40dcd65e5fb663d2",
+    "url": "/nt-greek/static/media/imperative_mood0_img.bfbf7032.JPG"
+  },
+  {
+    "revision": "42819a35e093abdbaf36ef0c624dd076",
+    "url": "/nt-greek/static/media/imperative_mood1_img.42819a35.JPG"
+  },
+  {
+    "revision": "6033a2da8093ffaa511863726b807ffe",
+    "url": "/nt-greek/static/media/interrogative_and_indefinite_pronouns0_img.6033a2da.JPG"
+  },
+  {
+    "revision": "2573f3289142472fae26880efca966e8",
+    "url": "/nt-greek/static/media/interrogative_and_indefinite_pronouns1_img.2573f328.JPG"
+  },
+  {
+    "revision": "64876fec482aa6109a8dd756a7ab8575",
+    "url": "/nt-greek/static/media/numerals_3_and4_img.64876fec.JPG"
+  },
+  {
+    "revision": "12acef8397e4fdb8bc53ffc89b4b1193",
+    "url": "/nt-greek/static/media/numerals_img.12acef83.JPG"
+  },
+  {
+    "revision": "2708b7ad0a030c4e2fe7fd2f261b4969",
+    "url": "/nt-greek/static/media/participles0_img.2708b7ad.JPG"
+  },
+  {
+    "revision": "710c69c50f872fbcba4e6df95e033cba",
+    "url": "/nt-greek/static/media/participles1_img.710c69c5.JPG"
+  },
+  {
+    "revision": "47c459c6ab00c1f58997cb31bf478a4a",
+    "url": "/nt-greek/static/media/participles2_img.47c459c6.JPG"
+  },
+  {
+    "revision": "d51edd0e1a16402172d43c2d0bde4912",
+    "url": "/nt-greek/static/media/participles3_img.d51edd0e.JPG"
+  },
+  {
+    "revision": "188c146ebf320b807fe261b076e7b574",
+    "url": "/nt-greek/static/media/participles4_img.188c146e.JPG"
+  },
+  {
+    "revision": "848bff7e395b455b7e0341b5f8889e4c",
+    "url": "/nt-greek/static/media/participles5_img.848bff7e.JPG"
+  },
+  {
+    "revision": "2fcca2f9e479c425e41470dbd5fea969",
+    "url": "/nt-greek/static/media/participles6_img.2fcca2f9.JPG"
+  },
+  {
+    "revision": "310cbe90270e910c951fdbfce6fd59b0",
+    "url": "/nt-greek/static/media/pas0_img.310cbe90.JPG"
+  },
+  {
+    "revision": "5baf71bfd43fd9838e786ad135755676",
+    "url": "/nt-greek/static/media/pas1_img.5baf71bf.JPG"
+  },
+  {
+    "revision": "fb2454a4010b4ced0ad755e2a4f17d3e",
+    "url": "/nt-greek/static/media/personal_pronoun_1st_img.fb2454a4.JPG"
+  },
+  {
+    "revision": "942ff188c4583bcedd4135655c9800e5",
+    "url": "/nt-greek/static/media/personal_pronoun_2nd_img.942ff188.JPG"
+  },
+  {
+    "revision": "81ee97ad4dddb2e04e39d5dfa7d93a0d",
+    "url": "/nt-greek/static/media/personal_pronoun_3rd_img.81ee97ad.JPG"
+  },
+  {
+    "revision": "8ab717640b536deb45a393daee3f2e0e",
+    "url": "/nt-greek/static/media/punctuation_img.8ab71764.JPG"
+  },
+  {
+    "revision": "37441913fe125119cd4f100ab1555c0e",
+    "url": "/nt-greek/static/media/reciprocal_pronoun_img.37441913.JPG"
+  },
+  {
+    "revision": "d25f93675cef9c22af214192a4b4dbdf",
+    "url": "/nt-greek/static/media/reflexive_pronouns0_img.d25f9367.JPG"
+  },
+  {
+    "revision": "f466f328843f8ae46471a827ae19112f",
+    "url": "/nt-greek/static/media/reflexive_pronouns1_img.f466f328.JPG"
+  },
+  {
+    "revision": "b4996edb08f7321d263df1a22e2773f1",
+    "url": "/nt-greek/static/media/reflexive_pronouns2_img.b4996edb.JPG"
+  },
+  {
+    "revision": "b7132ed262fa3979e754e5531ee8f204",
+    "url": "/nt-greek/static/media/relative_pronoun_img.b7132ed2.JPG"
+  },
+  {
+    "revision": "b4182771a5fc4aaec1191c20f32babf6",
+    "url": "/nt-greek/static/media/rule_of_noun_accent_img.b4182771.JPG"
+  },
+  {
+    "revision": "e67c2e4f52d16a15984b387d56982860",
+    "url": "/nt-greek/static/media/rule_of_verb_accent_img.e67c2e4f.JPG"
+  },
+  {
+    "revision": "276858bf11ad3521a49f1543c529ba81",
+    "url": "/nt-greek/static/media/second_declension_masc_img.276858bf.JPG"
+  },
+  {
+    "revision": "15b8edee6a659d4a105723fecf9976b5",
+    "url": "/nt-greek/static/media/second_declension_neuter_img.15b8edee.JPG"
+  },
+  {
+    "revision": "c3e2303ec700f438ca9ede1fb8796c92",
+    "url": "/nt-greek/static/media/third_declension0_img.c3e2303e.JPG"
+  },
+  {
+    "revision": "19571432036a9cb74191a63e7a21076d",
+    "url": "/nt-greek/static/media/third_declension1_img.19571432.JPG"
+  },
+  {
+    "revision": "46e7e2cf116a9746f52b765cbddffdf8",
+    "url": "/nt-greek/static/media/third_declension2_img.46e7e2cf.JPG"
+  },
+  {
+    "revision": "0cabf05771ea649010376357aa7a60eb",
+    "url": "/nt-greek/static/media/third_declension3_img.0cabf057.JPG"
+  },
+  {
+    "revision": "aa0e77c3c9c166ac42db6fec0668c44d",
+    "url": "/nt-greek/static/media/third_declension4_img.aa0e77c3.JPG"
+  },
+  {
+    "revision": "6dcbf492dd159e8cb5d9bdd30924b889",
+    "url": "/nt-greek/static/media/third_declension_img.6dcbf492.JPG"
+  },
+  {
+    "revision": "af8a4ad36636700b59c1abcd594235ed",
+    "url": "/nt-greek/static/media/third_declension_ua_img.af8a4ad3.JPG"
+  },
+  {
+    "revision": "200a95fbc1dc4a116719d5007cf3b70f",
+    "url": "/nt-greek/static/media/verbs_img.200a95fb.PNG"
+  },
+  {
+    "revision": "9e5de604195fd946269f33116466f3db",
+    "url": "/nt-greek/static/media/verbs_in_ui0.9e5de604.JPG"
+  },
+  {
+    "revision": "0b55ee495bbdac7fa9153a7200194ee0",
+    "url": "/nt-greek/static/media/verbs_in_ui1.0b55ee49.JPG"
+  },
+  {
+    "revision": "d0472b9dfc1f7c94aa0e753f6b715192",
+    "url": "/nt-greek/static/media/verbs_in_ui2.d0472b9d.JPG"
+  },
+  {
+    "revision": "f5d0d47df8f2854721d7bccc536ff86b",
+    "url": "/nt-greek/static/media/verbs_in_ui3.f5d0d47d.JPG"
+  },
+  {
+    "revision": "b55f8d8369fc377423370cc01b4ddc7b",
+    "url": "/nt-greek/static/media/verbs_in_ui4.b55f8d83.JPG"
+  },
+  {
+    "revision": "096e835b67a1c25232e17f851f055825",
+    "url": "/nt-greek/static/media/verbs_in_ui5.096e835b.JPG"
+  },
+  {
+    "revision": "349e17a4f2d049dc5e26fb32a33a93ad",
+    "url": "/nt-greek/static/media/vowels_img.349e17a4.JPG"
+  },
+  {
+    "revision": "a6da6c9e69817e749132bf03a5eefe9c",
+    "url": "/nt-greek/static/media/γράφω_second_aorist_passive_img.a6da6c9e.JPG"
+  },
+  {
+    "revision": "b26aa07214dda473084ba5688754d307",
+    "url": "/nt-greek/static/media/εἰμί_img.b26aa072.JPG"
+  },
+  {
+    "revision": "9f3d943f35b333dfc2e255a35d6d9e93",
+    "url": "/nt-greek/static/media/εἰμί_present_active_participle_img.9f3d943f.JPG"
+  },
+  {
+    "revision": "b221c022dcec14ca0558f6fbba612de0",
+    "url": "/nt-greek/static/media/πιστεύω_img.b221c022.JPG"
+  },
+  {
+    "revision": "4a88dd66035a787c0d880b0662210b63",
+    "url": "/nt-greek/static/media/ἔρχομαι_2nd_aorist_active_ind_img.4a88dd66.JPG"
+  },
+  {
+    "revision": "11ff3a2bfc9d8ad130245a3eb577cf7a",
+    "url": "/nt-greek/static/media/ἔρχομαι_img.11ff3a2b.JPG"
+  }
+]);
